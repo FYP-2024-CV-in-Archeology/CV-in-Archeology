a8652845c47a14ae077e68c333a7becf53bb0712
Yuanyang Tu <ryan@Yuanyangs-MacBook-Pro.local>
2024-03-30 14:45:49 +0800

    add various version of parallel pipeline

--- pipeline_parallel_v4.py
+++ pipeline_parallel_v4.py
@@ -1,20 +1,17 @@
 import os
 import sys
 import time
-import cProfile
 
 sys.path.append('../')
 
-import numpy as np
-import tkinter as tk
 from pathlib import Path
 import cv2 as cv
-import rawpy
 
 import multiprocessing
 from multiprocessing import Process
 from multiprocessing import Queue
-from multiprocessing import Pipe
+from multiprocessing import pool
+
 
 import utils
 from cropping import detectSherd, crop
@@ -22,32 +19,21 @@ from scaling import scaling, calc_scaling_ratio
 from color_correction import color_correction, percentile_whitebalance, imresize
 
 # process to read images from file system
-def imread(input_path, queue_out):
-    #print(f"imread Started!")
-    
-    for dirpath, dirnames, filenames in os.walk(input_path):
-        dirnames.sort()
-        for file in filenames:
-            path = Path(dirpath) / file
-            dir, filename = path.parent.name, path.stem
-            
-            if '.DS_Store' in filename or 'pxrf' in filename:
-                continue
-            filename = filename[0]
+def imread(queue_in, queue_out):
+    while True:
+        path = queue_in.get()
+        if path is None:
+            queue_out.put(path)
+            break
 
-            if dir > '478130_4419430_8_20':
-                if 'cr' in path.suffix.lower() and (filename == '2' or filename == '1'):
-                    queue_out.put([path, utils.imread(path)])
-                    #print(f"imread finished {path}")
-                    # sleep(999999.9999)
+        queue_out.put([path, utils.imread(path)])
 
-    #print(f"imread Done!")
-    queue_out.put(None)
-    queue_out.put(None)
+    print(f"imread finished {path}")
+    print(f"imread Done!")
 
 # process to detect 24 or 4 color checker
 def detect24(queue_in, queue_out):
-    #print(f"detect24 Started!")
+    print(f"detect24 Started!")
 
     while True:
         data = queue_in.get()
@@ -62,18 +48,18 @@ def detect24(queue_in, queue_out):
         scaling_ratio = calc_scaling_ratio(raw_img, is24Checker, 900)
 
         queue_out.put([path, raw_img, is24Checker, scaling_ratio]) 
-        #print(f"detect24 finished {path}")
-    #print(f"detect24 Done!")
+        print(f"detect24 finished {path}")
+    print(f"detect24 Done!")
 
 # process to whitebalance images and also detect sherd contour
-def whitebalance(queue_in, queue_out):
-    #print(f"Whitebalance Started!")
+def whitebalance(queue_in, queue_out, num):
+    print(f"Whitebalance Started!")
 
     while True:
         data = queue_in.get()
         if data is None:
-            queue_out.put(data)
-            queue_out.put(data)
+            for _ in range(num):
+                queue_out.put(data)
             break
         
         path, raw_img, is24, scalin_ratio = data
@@ -82,13 +68,13 @@ def whitebalance(queue_in, queue_out):
         sherd_cnt, patch_pos = detectSherd(white_balance_img, is24)
         rotation = utils.detect_rotation(white_balance_img, sherd_cnt, patch_pos)
         queue_out.put([path, white_balance_img, is24, scalin_ratio, sherd_cnt, rotation])
-        #print(f"Whitebalance finished {path}")
+        print(f"Whitebalance finished {path}")
 
-    #print(f"Whitebalance Done!")
+    print(f"Whitebalance Done!")
 
 # process to color correct images
 def color_correct(queue_in, queue_out):
-    #print(f"Color Correction Started!")
+    print(f"Color Correction Started!")
 
     while True:
         data = queue_in.get()
@@ -101,15 +87,15 @@ def color_correct(queue_in, queue_out):
         is24Checker = utils.detect24Checker(cv.cvtColor(white_balance_img, cv.COLOR_RGB2BGR), detector)
         color_correct_img = color_correction(white_balance_img, detector, is24Checker)
         queue_out.put([path, color_correct_img, is24, scalin_ratio, sherd_cnt, rotation])
-        #print(f"Color Corrected {path}")
+        print(f"Color Corrected {path}")
 
-    #print(f"Color Correction Done!")
+    print(f"Color Correction Done!")
 
 # do the last processing of images and write them to file system
-def imwrite(queue_in, sizes):
-    #print(f"Whitebalance Started!")
+def imwrite(queue_in, sizes, num):
+    print(f"Whitebalance Started!")
     done = 0
-    while done < 2:
+    while done < num:
         data = queue_in.get()
         if data is None:
             done += 1
@@ -125,32 +111,63 @@ def imwrite(queue_in, sizes):
         cropped_img = scaling(crop(processed_img, sherd_cnt, scalin_ratio), scalin_ratio)
         cv.imwrite(f"outputs/{path.parent.parent.name}_{path.stem}.jpg", cropped_img)
 
-        #print(f"Write finished {path}")
-    #print(f"imwrite Done!")
+        print(f"Write finished {path}")
+    print(f"imwrite Done!")
 
+def read_path(input_path, num, manager):
+    queue = manager.Queue()   
+    for dirpath, dirnames, filenames in os.walk(input_path):
+        dirnames.sort()
+        for file in filenames:
+            path = Path(dirpath) / file
+            dir, filename = path.parent.name, path.stem
+            
+            if '.DS_Store' in filename or 'pxrf' in filename:
+                continue
+            filename = filename[0]
+
+            if dir > '478130_4419430_8_20':
+                if 'cr' in path.suffix.lower() and (filename == '2' or filename == '1'):
+                    queue.put(path)
+    for _ in range(num):
+        queue.put(None)
+
+    return queue
 
 if __name__ == "__main__":
     start_time = time.time()
-    # create pipes
     sizes = {1000}
-    queues = [Queue(maxsize=2) for _ in range(4)]
-    lock = multiprocessing.Lock()
-    # create processes
-    processes = [
-        Process(target=imread, args=('test', queues[0])),
-        Process(target=detect24, args=(queues[0], queues[1])),
-        Process(target=whitebalance, args=(queues[1], queues[2])),
-        Process(target=color_correct, args=(queues[2], queues[3])),
-        Process(target=color_correct, args=(queues[2], queues[3])),
-        Process(target=imwrite, args=(queues[3], sizes)),
-        # Process(target=imwrite, args=(queues[4], sizes))
-    ]
-    
-    for process in processes:
-        process.start()
-    
-    # see if a process is done
-    for process in processes:
-        process.join()
+
+    with multiprocessing.Manager() as manager:
+        num = 2
+        pathes_queue = read_path('test', num, manager)
+
+        # create pipes
+        queues = [manager.Queue() for _ in range(4)]
+        # with multiprocessing.Pool(6) as pool:
+        imread_pool = pool.Pool(2)
+        detect24_pool = pool.Pool(2)
+        whitebalance_pool = pool.Pool(2)
+        color_correct_pool = pool.Pool(4)
+        imwrite_pool = pool.Pool(2)
+
+        imread_pool.starmap_async(imread, [(pathes_queue, queues[0]) for _ in range(2)])
+        detect24_pool.starmap_async(detect24, [(queues[0], queues[1]) for _ in range(2)])
+        whitebalance_pool.starmap_async(whitebalance, [(queues[1], queues[2], num) for _ in range(2)])
+        color_correct_pool.starmap_async(color_correct, [(queues[2], queues[3]) for _ in range(4)])
+        imwrite_pool.starmap_async(imwrite, [(queues[3], sizes, num) for _ in range(2)])
+
+        imread_pool.close()
+        detect24_pool.close()
+        whitebalance_pool.close()
+        color_correct_pool.close()
+        imwrite_pool.close()
+        
+        imread_pool.join()
+        detect24_pool.join()
+        whitebalance_pool.join()
+        color_correct_pool.join()
+        imwrite_pool.join()
+        
     
     print("finished in {} seconds".format(time.time() - start_time))
